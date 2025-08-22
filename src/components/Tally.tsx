15a3aacdb3e5cbacaca6144d0d7495774c11a7fd
Patryk Majewski <patrykmajewski177@gmail.com>
2025-08-22 12:04:04 +0200

    added table tally

--- src/components/Tally.tsx
+++ src/components/Tally.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Tally = () => {
     return (
-        <div> TO Tally</div>
+        <div>DDD</div>
     )
 }
--- src/pages/TallyListPage.tsx
+++ src/pages/TallyListPage.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 import {Tally} from "../components/Tally";
-
+import "../styles/tallyListPage.scss";
 
 export const TallyListPage = (props: any) => {
     return (
-        <Tally />
+        <table className="global--container table">
+            <thead>
+            <tr>
+                <th>Co wymieniono</th>
+                <th>Data wymiany</th>
+                <th>Marka części</th>
+                <th>Cena</th>
+                <th>Gdzie servisowano</th>
+                <th>Imię mechanika</th>
+                <th>Gwarancja</th>
+                <th>Koniec gwarancji</th>
+                <th>Aktualny przebieg</th>
+                <th>Przebieg przed naprawą</th>
+                <th>Gwarancja wg przebiegu</th>
+                <th>Nazwa dokumentu</th>
+                <th>Adres URL dokumentu</th>
+                <th>Uwagi</th>
+            </tr>
+            <tbody>
+                <tr></tr>
+            </tbody>
+            </thead>
+        </table>
+
     )
 }
