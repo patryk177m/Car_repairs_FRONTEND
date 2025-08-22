4aae8f17b3ed7e21f2bce9ffddaff5057db69b01
Patryk Majewski <patrykmajewski177@gmail.com>
2025-08-22 11:20:40 +0200

    rename files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import  React from 'react';
-import {FormAddTally} from "../components/formAddTally";
 
 export const Home = () => {
     return (
         <div>
             <h1>Home - page</h1>
-            <FormAddTally />
         </div>
     )
 }
