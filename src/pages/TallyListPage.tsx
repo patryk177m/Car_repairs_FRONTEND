ceff29ceccbaf06b5f11376a0b6ff4c40c8d1c03
Patryk Majewski <patrykmajewski177@gmail.com>
2025-08-23 07:30:23 +0200

    added map tallies

--- src/pages/TallyListPage.tsx
+++ src/pages/TallyListPage.tsx
@@ -1,31 +1,54 @@
-import React from "react";
-import {Tally} from "../components/Tally";
+import { useState, useEffect } from "react";
 import "../styles/tallyListPage.scss";
+import { TallyType } from "../types/tally";
+import { getTallies } from "../utils/api";
+import { Tally } from "../components/Tally";
 
-export const TallyListPage = (props: any) => {
+export const TallyListPage = () => {
+    const [tallies, setTallies] = useState<TallyType[]>([]);
+
+    useEffect(() => {
+        const fetchTallies = async () => {
+            try {
+                const response = await getTallies();
+                setTallies(response);
+                // return response
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        fetchTallies();
+    }, []);
+
     return (
         <table className="global--container table">
             <thead>
-            <tr>
-                <th>Co wymieniono</th>
-                <th>Data wymiany</th>
-                <th>Marka części</th>
-                <th>Cena</th>
-                <th>Gdzie servisowano</th>
-                <th>Imię mechanika</th>
-                <th>Gwarancja</th>
-                <th>Koniec gwarancji</th>
-                <th>Aktualny przebieg</th>
-                <th>Przebieg przed naprawą</th>
-                <th>Gwarancja wg przebiegu</th>
-                <th>Nazwa dokumentu</th>
-                <th>Adres URL dokumentu</th>
-                <th>Uwagi</th>
-            </tr>
-            <tbody>
-                <tr></tr>
-            </tbody>
+                <tr>
+                    <th>Co wymieniono</th>
+                    <th>Data wymiany</th>
+                    <th>Marka części</th>
+                    <th>Cena</th>
+                    <th>Gdzie servisowano</th>
+                    <th>Imię mechanika</th>
+                    <th>Gwarancja</th>
+                    <th>Koniec gwarancji</th>
+                    <th>Aktualny przebieg</th>
+                    <th>Przebieg przed naprawą</th>
+                    <th>Gwarancja wg przebiegu</th>
+                    <th>Dokumentu</th>
+                    <th colSpan={2}>Uwagi</th>
+                </tr>
             </thead>
+            <tbody>
+                {tallies.length <= 0 || tallies.map((v) => {
+                    return (<Tally
+                        tally={v}
+                        key={v.id}
+                    />)
+                })}
+            </tbody>
+
         </table>
 
     )
