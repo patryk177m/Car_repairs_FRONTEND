a9a5ad265192b29768ace4859b95a84ebd236a8a
Patryk Majewski <patrykmajewski177@gmail.com>
2025-08-24 07:29:20 +0200

    remove required for input

--- src/pages/FormAddTally.tsx
+++ src/pages/FormAddTally.tsx
@@ -38,7 +38,7 @@ export const FormAddTally: React.FC = () => {
             <input className="form__input" onChange={handleChange} value={String(tally?.date_replaced)} type="date" id="date_replaced" name="date_replaced" required/>
 
             <label className="form__label" htmlFor="part_brand"> Marka części </label>
-            <input className="form__input" onChange={handleChange} value={tally?.part_brand} type="text" id="part_brand" name="part_brand" required/>
+            <input className="form__input" onChange={handleChange} value={tally?.part_brand} type="text" id="part_brand" name="part_brand" />
 
             <label className="form__label" htmlFor="cost"> Cena </label>
             <input className="form__input" onChange={handleChange} value={tally?.cost} type="text" id="cost" name="cost" required/>
@@ -47,31 +47,31 @@ export const FormAddTally: React.FC = () => {
             <input className="form__input" onChange={handleChange} value={tally?.service} type="text" id="service" name="service" required/>
 
             <label className="form__label" htmlFor="mechanic"> Imię mechanika </label>
-            <input className="form__input" onChange={handleChange} value={tally?.mechanic} type="text" id="mechanic" name="mechanic" required/>
+            <input className="form__input" onChange={handleChange} value={tally?.mechanic} type="text" id="mechanic" name="mechanic" />
 
             <label className="form__label" htmlFor="guarantee"> Czy gwarancja </label>
-            <input className="form__input" onChange={handleChange} value={String(tally?.guarantee) === "undefined" ? "" : String(tally?.guarantee)} type="text" id="guarantee" name="guarantee" required/>
+            <input className="form__input" onChange={handleChange} value={String(tally?.guarantee) === "undefined" ? "" : String(tally?.guarantee)} type="text" id="guarantee" name="guarantee" />
 
             <label className="form__label" htmlFor="guarantee_time"> Czas do końca gwarancji </label>
-            <input className="form__input" onChange={handleChange} value={String(tally?.guarantee_time)} type="date" id="guarantee_time" name="guarantee_time" required/>
+            <input className="form__input" onChange={handleChange} value={String(tally?.guarantee_time)} type="date" id="guarantee_time" name="guarantee_time" />
 
             <label className="form__label" htmlFor="current_mileage"> Aktualny przebieg </label>
-            <input className="form__input" onChange={handleChange} value={tally?.current_mileage} type="text" id="current_mileage" name="current_mileage" required/>
+            <input className="form__input" onChange={handleChange} value={tally?.current_mileage} type="text" id="current_mileage" name="current_mileage" />
 
             <label className="form__label" htmlFor="mileage_before_service"> Przebieg przed naprawą </label>
             <input className="form__input" onChange={handleChange} value={tally?.mileage_before_service} type="text" id="mileage_before_service" name="mileage_before_service" required/>
 
             <label className="form__label" htmlFor="warranty_by_mileage"> Gwarancja wg przebiegu </label>
-            <input className="form__input" onChange={handleChange} value={tally?.warranty_by_mileage} type="text" id="warranty_by_mileage" name="warranty_by_mileage" required/>
+            <input className="form__input" onChange={handleChange} value={tally?.warranty_by_mileage} type="text" id="warranty_by_mileage" name="warranty_by_mileage" />
 
-            <label className="form__label" htmlFor="document_title"> Nazwa dokumentu </label>
-            <input className="form__input" onChange={handleChange} value={tally?.document_title} type="text" id="document_title" name="document_title" required/>
+            <label className="form__label" htmlFor="document_title"> Dodaj dokument </label>
+            <input className="form__input" onChange={handleChange} value={tally?.document_title} type="file" id="document_title" name="document_title" />
 
             <label className="form__label" htmlFor="documentURL"> Adres URL dokumentu </label>
-            <input className="form__input" onChange={handleChange} value={tally?.documentURL} type="text" id="documentURL" name="documentURL" required/>
+            <input className="form__input" onChange={handleChange} value={tally?.documentURL} type="text" id="documentURL" name="documentURL" />
 
             <label className="form__label" htmlFor="comments"> Uwagi </label>
-            <textarea className="form__textarea" onChange={handleChange} value={tally?.comments} id="comments" name="comments" required/>
+            <textarea className="form__textarea" onChange={handleChange} value={tally?.comments} id="comments" name="comments" />
 
             <button className="form__button" type="submit">Zapisz</button>
         </form>
