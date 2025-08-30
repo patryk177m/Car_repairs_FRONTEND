ab27d1059e1aef573e2020e2cd29b68787936813
Patryk Majewski <patrykmajewski177@gmail.com>
2025-08-30 11:09:58 +0200

    fix

--- src/components/TallyUpdate.tsx
+++ src/components/TallyUpdate.tsx
@@ -41,6 +41,10 @@ export const FileUpdate = () => {
         getTallyById(id).then((data) => {
             if (!data) return;
             setSelectedTally(data)
+        }).finally(() => {
+            setFile(null);
+            setFileName("");
+            selectedTally.documentURL = "";
         });
     }, []);
 
@@ -50,51 +54,51 @@ export const FileUpdate = () => {
             <form className="form--container form global--container" onSubmit={onSubmit}>
                 <label className="form__label" htmlFor="replaced"> Co wymieniono </label>
                 <input className="form__input" onChange={e => handleChange(e, setSelectedTally)}
-                       value={selectedTally?.replaced} type="text" id="replaced" name="replaced" required/>
+                       value={selectedTally.replaced} type="text" id="replaced" name="replaced" required/>
 
                 <label className="form__label" htmlFor="date_replaced"> Data wymiany </label>
                 <input className="form__input" onChange={e => handleChange(e, setSelectedTally)}
-                       value={String(selectedTally?.date_replaced) ? new Date(selectedTally.date_replaced).toISOString().split("T")[0]
+                       value={String(selectedTally.date_replaced) ? new Date(selectedTally.date_replaced).toISOString().split("T")[0]
                            : ""} type="date" id="date_replaced" name="date_replaced" required/>
 
                 <label className="form__label" htmlFor="part_brand"> Marka części </label>
                 <input className="form__input" onChange={e => handleChange(e, setSelectedTally)}
-                       value={selectedTally?.part_brand} type="text" id="part_brand" name="part_brand"/>
+                       value={selectedTally.part_brand} type="text" id="part_brand" name="part_brand"/>
 
                 <label className="form__label" htmlFor="cost"> Cena </label>
                 <input className="form__input" onChange={e => handleChange(e, setSelectedTally)}
-                       value={selectedTally?.cost} type="text" id="cost" name="cost" required/>
+                       value={selectedTally.cost} type="text" id="cost" name="cost" required/>
 
                 <label className="form__label" htmlFor="service"> Gdzie naprawiano </label>
                 <input className="form__input" onChange={e => handleChange(e, setSelectedTally)}
-                       value={selectedTally?.service} type="text" id="service" name="service" required/>
+                       value={selectedTally.service} type="text" id="service" name="service" required/>
 
                 <label className="form__label" htmlFor="mechanic"> Imię mechanika </label>
                 <input className="form__input" onChange={e => handleChange(e, setSelectedTally)}
-                       value={selectedTally?.mechanic} type="text" id="mechanic" name="mechanic"/>
+                       value={selectedTally.mechanic} type="text" id="mechanic" name="mechanic"/>
 
                 <label className="form__label" htmlFor="guarantee"> Czy gwarancja </label>
                 <input className="form__input" onChange={e => handleChange(e, setSelectedTally)}
-                       value={String(selectedTally?.guarantee) === "undefined" ? "" : String(selectedTally?.guarantee)}
+                       value={String(selectedTally.guarantee) === "undefined" ? "" : String(selectedTally?.guarantee)}
                        type="text" id="guarantee" name="guarantee"/>
 
                 <label className="form__label" htmlFor="guarantee_time"> Czas do końca gwarancji </label>
                 <input className="form__input" onChange={e => handleChange(e, setSelectedTally)}
-                       value={String(selectedTally?.guarantee_time) ? new Date(selectedTally.guarantee_time).toISOString().split("T")[0]
+                       value={String(selectedTally.guarantee_time) ? new Date(selectedTally.guarantee_time).toISOString().split("T")[0]
                            : ""} type="date" id="guarantee_time" name="guarantee_time"/>
 
                 <label className="form__label" htmlFor="current_mileage"> Aktualny przebieg </label>
                 <input className="form__input" onChange={e => handleChange(e, setSelectedTally)}
-                       value={selectedTally?.current_mileage} type="text" id="current_mileage" name="current_mileage"/>
+                       value={selectedTally.current_mileage} type="text" id="current_mileage" name="current_mileage"/>
 
                 <label className="form__label" htmlFor="mileage_before_service"> Przebieg przed naprawą </label>
                 <input className="form__input" onChange={e => handleChange(e, setSelectedTally)}
-                       value={selectedTally?.mileage_before_service} type="text" id="mileage_before_service"
+                       value={selectedTally.mileage_before_service} type="text" id="mileage_before_service"
                        name="mileage_before_service" required/>
 
                 <label className="form__label" htmlFor="warranty_by_mileage"> Gwarancja wg przebiegu </label>
                 <input className="form__input" onChange={e => handleChange(e, setSelectedTally)}
-                       value={selectedTally?.warranty_by_mileage} type="text" id="warranty_by_mileage"
+                       value={selectedTally.warranty_by_mileage} type="text" id="warranty_by_mileage"
                        name="warranty_by_mileage"/>
 
                 <label className="form__label label--added--file" htmlFor="document_title">Dodaj plik</label>
@@ -105,12 +109,12 @@ export const FileUpdate = () => {
                     <p>{message}</p>
                 </label>
                 <label className="form__label" htmlFor="documentURL"> Adres URL dokumentu </label>
-                <input className="form__input" value={selectedTally?.documentURL || fileName} onChange={e => handleChange(e, setSelectedTally)}
+                <input className="form__input" value={selectedTally.documentURL || fileName} onChange={e => handleChange(e, setSelectedTally)}
                        type="text" id="documentURL" name="documentURL"/>
 
                 <label className="form__label" htmlFor="comments"> Uwagi </label>
                 <textarea className="form__textarea" onChange={e => handleChange(e, setSelectedTally)}
-                          value={selectedTally?.comments} id="comments" name="comments"/>
+                          value={selectedTally.comments} id="comments" name="comments"/>
 
                 <button className="form__button" type="submit">Zaktualizuj</button>
             </form>
