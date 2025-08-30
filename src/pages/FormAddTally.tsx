2783c1799dcb99fc1acef31225a364ba2bf4750c
Patryk Majewski <patrykmajewski177@gmail.com>
2025-08-30 11:35:30 +0200

    added navigate to /list

--- src/pages/FormAddTally.tsx
+++ src/pages/FormAddTally.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
-import  "../styles/formAddTally.scss";
+import React, {useState} from "react";
+import "../styles/formAddTally.scss";
 import "../styles/global.scss";
 import {createTally} from "../utils/api";
 import {TallyType} from "../types/tally";
 import {useTallyContext} from "../context/TallyContext";
 import {handleFileChange, uploadFile} from "../utils/utils";
+import {useNavigate} from "react-router";
 
 export const FormAddTally = () => {
     const now = new Date();
+    const navigate = useNavigate();
     const [tally, setTally] = useState<Omit<TallyType, "id">>({
         replaced: "",
         date_replaced: now,
@@ -24,65 +26,86 @@ export const FormAddTally = () => {
         document_title: "",
         documentURL: "",
     });
-    const { file, setFile, fileName, setFileName, message, setMessage, handleChange } = useTallyContext();
+    const {file, setFile, fileName, setFileName, message, setMessage, handleChange} = useTallyContext();
 
     const onSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
-       e.preventDefault();
+        e.preventDefault();
         if (!tally) return;
-        await uploadFile(file, setMessage).then((data: string) => createTally({...tally, documentURL: data })).finally(() => {
-                setFileName("");
-                setFile(null);
-                tally.documentURL = "";
-            }
-        );
+        await uploadFile(file, setMessage)
+            .then((data: string) => {
+                createTally({...tally, documentURL: data});
+                navigate("/list")
+            })
+            .finally(() => {
+                    setFileName("");
+                    setFile(null);
+                    tally.documentURL = "";
+                }
+            );
     }
 
     return (
         <form className="form--container form global--container" onSubmit={onSubmit}>
             <label className="form__label" htmlFor="replaced"> Co wymieniono </label>
-            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.replaced} type="text" id="replaced" name="replaced" required/>
+            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.replaced} type="text"
+                   id="replaced" name="replaced" required/>
 
             <label className="form__label" htmlFor="date_replaced"> Data wymiany </label>
-            <input className="form__input" onChange={e => handleChange(e, setTally)} value={String(tally?.date_replaced)} type="date" id="date_replaced" name="date_replaced" required/>
+            <input className="form__input" onChange={e => handleChange(e, setTally)}
+                   value={String(tally?.date_replaced)} type="date" id="date_replaced" name="date_replaced" required/>
 
             <label className="form__label" htmlFor="part_brand"> Marka części </label>
-            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.part_brand} type="text" id="part_brand" name="part_brand" />
+            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.part_brand}
+                   type="text" id="part_brand" name="part_brand"/>
 
             <label className="form__label" htmlFor="cost"> Cena </label>
-            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.cost} type="text" id="cost" name="cost" required/>
+            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.cost} type="text"
+                   id="cost" name="cost" required/>
 
             <label className="form__label" htmlFor="service"> Gdzie naprawiano </label>
-            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.service} type="text" id="service" name="service" required/>
+            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.service} type="text"
+                   id="service" name="service" required/>
 
             <label className="form__label" htmlFor="mechanic"> Imię mechanika </label>
-            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.mechanic} type="text" id="mechanic" name="mechanic" />
+            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.mechanic} type="text"
+                   id="mechanic" name="mechanic"/>
 
             <label className="form__label" htmlFor="guarantee"> Czy gwarancja </label>
-            <input className="form__input" onChange={e => handleChange(e, setTally)} value={String(tally?.guarantee) === "undefined" ? "" : String(tally?.guarantee)} type="text" id="guarantee" name="guarantee" />
+            <input className="form__input" onChange={e => handleChange(e, setTally)}
+                   value={String(tally?.guarantee) === "undefined" ? "" : String(tally?.guarantee)} type="text"
+                   id="guarantee" name="guarantee"/>
 
             <label className="form__label" htmlFor="guarantee_time"> Czas do końca gwarancji </label>
-            <input className="form__input" onChange={e => handleChange(e, setTally)} value={String(tally?.guarantee_time)} type="date" id="guarantee_time" name="guarantee_time" />
+            <input className="form__input" onChange={e => handleChange(e, setTally)}
+                   value={String(tally?.guarantee_time)} type="date" id="guarantee_time" name="guarantee_time"/>
 
             <label className="form__label" htmlFor="current_mileage"> Aktualny przebieg </label>
-            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.current_mileage} type="text" id="current_mileage" name="current_mileage" />
+            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.current_mileage}
+                   type="text" id="current_mileage" name="current_mileage"/>
 
             <label className="form__label" htmlFor="mileage_before_service"> Przebieg przed naprawą </label>
-            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.mileage_before_service} type="text" id="mileage_before_service" name="mileage_before_service" required/>
+            <input className="form__input" onChange={e => handleChange(e, setTally)}
+                   value={tally?.mileage_before_service} type="text" id="mileage_before_service"
+                   name="mileage_before_service" required/>
 
             <label className="form__label" htmlFor="warranty_by_mileage"> Gwarancja wg przebiegu </label>
-            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.warranty_by_mileage} type="text" id="warranty_by_mileage" name="warranty_by_mileage" />
+            <input className="form__input" onChange={e => handleChange(e, setTally)} value={tally?.warranty_by_mileage}
+                   type="text" id="warranty_by_mileage" name="warranty_by_mileage"/>
 
             <label className="form__label label--added--file" htmlFor="document_title">Dodaj plik</label>
             <label className="form__label label--added" htmlFor="document_title">
                 <img className="icon__upload" src="/img/wgrywanie.png" alt="wgrywanie"/>
-                <input className="form__input input--added"  onChange={(e) => handleFileChange(e, setFile, setFileName)} type="file" id="document_title" name="document_title" />
+                <input className="form__input input--added" onChange={(e) => handleFileChange(e, setFile, setFileName)}
+                       type="file" id="document_title" name="document_title"/>
                 <p>{message}</p>
             </label>
             <label className="form__label" htmlFor="documentURL"> Adres URL dokumentu </label>
-            <input className="form__input" onChange={e => handleChange(e, setTally)} value={fileName} type="text" id="documentURL" name="documentURL" />
+            <input className="form__input" onChange={e => handleChange(e, setTally)} value={fileName} type="text"
+                   id="documentURL" name="documentURL"/>
 
             <label className="form__label" htmlFor="comments"> Uwagi </label>
-            <textarea className="form__textarea" onChange={e => handleChange(e, setTally)} value={tally?.comments} id="comments" name="comments" />
+            <textarea className="form__textarea" onChange={e => handleChange(e, setTally)} value={tally?.comments}
+                      id="comments" name="comments"/>
 
             <button className="form__button" type="submit">Zapisz</button>
         </form>
