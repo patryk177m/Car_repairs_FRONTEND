2e94371f6d8901622253364ba968d69efc952389
patryk177m <patrykmajewski177@gmail.com>
2025-09-02 06:31:00 +0200

    positioning form login view

--- src/pages/FormLogin.tsx
+++ src/pages/FormLogin.tsx
@@ -4,14 +4,16 @@ import "../styles/global.scss";
 
 export const FormLogin = () => {
     return (
-        <form className="form--container form global--container">
-            <label className="form__label" htmlFor="email" >Email Address</label>
-                <input className="form__input" id="email" type="email" name="email" required />
+        <main className="main">
+            <form className="form--container form global--container">
+                <label className="form__label" htmlFor="email">Email Address</label>
+                <input className="form__input" id="email" type="email" name="email" required/>
 
-            <label className="form__label" htmlFor="password">Hasło</label>
-                <input className="form__input" id="password" type="password" name="password" required />
+                <label className="form__label" htmlFor="password">Hasło</label>
+                <input className="form__input" id="password" type="password" name="password" required/>
 
-            <button className="form__button" type="submit">Login</button>
-        </form>
+                <button className="form__button" type="submit">Login</button>
+            </form>
+        </main>
     )
 }
