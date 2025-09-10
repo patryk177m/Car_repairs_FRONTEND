e37e6acd6f11211a786c9a1c0f6564d850356b87
patryk177m <patrykmajewski177@gmail.com>
2025-09-10 02:28:06 +0200

    added form register

--- src/pages/FormRegister.tsx
+++ src/pages/FormRegister.tsx
@@ -4,7 +4,7 @@ import "../styles/global.scss";
 import {User} from "../types/user";
 import {useTallyContext} from "../context/TallyContext";
 
-export const FormLogin = () => {
+export const FormRegister = () => {
     const {handleChange} = useTallyContext()
     const [user, setUser] = useState<Omit<User, "id">>({
         email: "",
@@ -19,8 +19,8 @@ export const FormLogin = () => {
                 <input onChange={(e) => handleChange(e, setUser)} value={user.email} className="form__input" id="email" type="email" name="email" required/>
                 <label className="form__label" htmlFor="password">Hasło</label>
                 <input onChange={(e) => handleChange(e,setUser)} value={user.password} className="form__input" id="password" type="password" name="password" required/>
-                {/*<label className="form__label" htmlFor="passwordConfirm">Powtórz hasło</label>*/}
-                {/*<input onChange={(e) => handleChange(e,setUser)} value={user.passwordConfirm} className="form__input" id="passwordConfirm" type="password" name="passwordConfirm" required/>*/}
+                <label className="form__label" htmlFor="passwordConfirm">Powtórz hasło</label>
+                <input onChange={(e) => handleChange(e,setUser)} value={user.passwordConfirm} className="form__input" id="passwordConfirm" type="password" name="passwordConfirm" required/>
 
                 <button className="form__button" type="submit">Login</button>
             </form>
