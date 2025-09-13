265e5d0ae1c2a8c35136a7b083aa1b84a777f207
patryk177m <patrykmajewski177@gmail.com>
2025-09-13 14:04:13 +0200

    hide menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import {useTallyContext} from "../context/TallyContext";
 
 export const Header: React.FC = () => {
     const navigate = useNavigate();
-    const { localToken, setLocalToken, handleLogout } = useTallyContext()
+    const {localToken, setLocalToken, handleLogout} = useTallyContext()
 
     useEffect(() => {
         setLocalToken(localStorage.getItem("token"));
@@ -15,12 +15,15 @@ export const Header: React.FC = () => {
         <nav className="nav ">
             <div className="nav__links global--container">
                 <Link className="link" to="/">Strona główna</Link>
-                <Link className="link" to="/list"> Zestawienie serwisowe</Link>
-                <Link className="link" to="/add">Dodaj usługę</Link>
-
-                { !localToken ?
-                <Link className="link" to="/login">Login</Link> :
-                <Link onClick={(e) => handleLogout(e, navigate)} className="link" to="/login">Logout</Link>
+                {localToken &&
+                    <>
+                        <Link className="link" to="/list"> Zestawienie serwisowe</Link>
+                        <Link className="link" to="/add">Dodaj usługę</Link>
+                    </>
+                }
+                {!localToken ?
+                    <Link className="link" to="/login">Login</Link> :
+                    <Link onClick={(e) => handleLogout(e, navigate)} className="link" to="/login">Logout</Link>
                 }
             </div>
 
