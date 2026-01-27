1804d8b7d3fb79fc7f40a5174380713af0723ca8
patryk177m <patrykmajewski177@gmail.com>
2026-01-27 03:29:30 +0100

    added styles and change input currentMileage

--- src/components/FormAddCurrentlyMileage.tsx
+++ src/components/FormAddCurrentlyMileage.tsx
@@ -1,26 +1,55 @@
-import React, {FormEvent, useState} from "react";
+import "../styles/FormAddCurrentlyMileage.scss";
+import React, {FormEvent, useEffect, useRef, useState} from "react";
 import {useTallyContext} from "../context/TallyContext";
 
 export const FormAddCurrentlyMileage = () => {
     const { valueCurrentMileage, setValueCurrentMileage} = useTallyContext()
-    const [currentlyMileage, setCurrentlyMileage] = useState<string>("")
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [disable, setDisable] = useState<boolean>(false);
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
     }
 
-    const handleClick = () => {
-        if (isNaN(Number(currentlyMileage))) return setCurrentlyMileage("")
-        setValueCurrentMileage(Number(currentlyMileage))
-        setCurrentlyMileage("")
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            setValueCurrentMileage(Number(e.currentTarget.value));
+            setDisable(true);
+        }
+    };
+
+    const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+        setValueCurrentMileage(Number(e.currentTarget.value));
+        if(valueCurrentMileage <= 0) return;
+        setDisable(true);
+    };
+
+    const handleDoubleClick = (e:  React.MouseEvent<HTMLLabelElement>) => {
+        setDisable(false);
+        setTimeout(() => {
+            inputRef.current!.focus();
+        }, 200)
     }
+
+    useEffect(() => {
+        inputRef.current!.focus();
+    }, [valueCurrentMileage]);
 
     return (
         <form onSubmit={handleSubmit} className="form_currently_mileage">
-            <label>Aktualny przebieg:</label>
-            <input className="currently_mileage__input" type="text" value={valueCurrentMileage} name="currlentlyMileageDisabled" disabled={true}/>
-            <input className="currently_mileage__input" onChange={e => {setCurrentlyMileage(e.target.value)}} value={currentlyMileage} type="text" name="currentlyMileage" placeholder="Wprowadź Aktualny przebieg:" />
-            <button className="form__button--currently_mileage" onClick={handleClick} type="submit">+</button>
+            <label className="currently_mileage__label" onDoubleClick={handleDoubleClick}>Aktualny przebieg:</label>
+            {!disable ? (
+                    <input ref={inputRef}
+                           onKeyDown={handleKeyDown}
+                           onBlur={handleBlur}
+                           className="currently_mileage__input currently_mileage__input--green"
+                           type="number"
+                           name="currlentlyMileageDisabled"
+                    />
+                ) : (
+                <input className="currently_mileage__input currently_mileage__input--red" type="number" disabled />
+                )
+            }
         </form>
     )
 }
