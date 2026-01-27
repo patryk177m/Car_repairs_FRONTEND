1f9a8bc06345a3c9fef236ff74d5e65c2c264e90
patryk177m <patrykmajewski177@gmail.com>
2026-01-27 03:31:55 +0100

    added picture

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,8 @@ import "../styles/home.scss"
 export const Home = () => {
     return (
         <main className="main global--container">
-            <h1 className="main__title">Zestawienie serisowe pojazdu osobowego</h1>
+            <h1 className="main__title">Zestawienie serwisowe pojazdu osobowego</h1>
+            <img className="main__photo global--container" src="/img/auto.png" alt="auto"/>
         </main>
     )
 }
